994b7cb7e05909b3a714a3055479d9c1eb63c56c
Choi Donghwan <dhchoi7117@gmail.com>
2021-01-15 14:28:54 +0900

    d

---
+++ main.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+from random import *
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import math
+import time
+from point import*
+from line import*
+from circle import*
+from gui import*
+
+    
+if __name__ == "__main__":
+    
+    
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
+
+    
